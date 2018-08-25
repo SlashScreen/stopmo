24d8d88163e6a7e077d18409bd876e985e668ffb
GJ <TALLESTTOWER1@GMAIL.COM>
2018-08-25 13:57:14 -0700

    added render, mdata is now dict, saves as such

--- inbetween.py
+++ inbetween.py
@@ -1,5 +1,12 @@
 from PIL import Image
+import cbtl_base
 
-imgs = [Image.open("intro1.png"),Image.open("intro2.png")]
-tween = Image.blend(imgs[0],imgs[1],.7)
-tween.show()
+def generateInbetweens(tl,a = .5):
+    for f in range(len(tl.get())):
+        tween = Image.blend(tl.get(f-1),tl.get(f),a)
+        tl.insert(f,tween)
+    tl.changeFPS(tl.getMData()["fps"]*2)
+
+test = cbtl_base.cowboytimeline(contents = [Image.open("intro1.png"),Image.open("intro2.png")])
+generateInbetweens(test)
+test.render("./tl","./out/tween.mp4")
